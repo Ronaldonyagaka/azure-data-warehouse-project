d51591c64904a7fb44eb239692a27ade3d0e08b5
RONALD ONGORI NYAGAKA <117894650+Ronaldonyagaka@users.noreply.github.com>
2025-07-29 22:23:26 +0300

    Update Create Views Gold.sql

--- Scripts/Create Views Gold.sql
+++ Scripts/Create Views Gold.sql
@@ -1,91 +1,75 @@
-CREATE VIEW gold.calendar
-AS
-SELECT 
-    *
-FROM
-    OPENROWSET(
-        BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Calender/',
-        FORMAT = 'PARQUET'
-    ) AS CalendarQuery
+-- =============================================
+-- Create Gold Views from Silver Layer Parquet Files
+-- Data Source: Azure Data Lake (Silver Layer)
+-- =============================================
+
+-- Calendar View
+CREATE VIEW gold.calendar AS
+SELECT *
+FROM OPENROWSET(
+    BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Calender/',
+    FORMAT = 'PARQUET'
+) AS CalendarQuery;
 
 
-CREATE VIEW gold.customer
-AS
-SELECT 
-    *
-FROM
-    OPENROWSET(
-        BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Customer/',
-        FORMAT = 'PARQUET'
-    ) AS CustomerQuery
+-- Customer View
+CREATE VIEW gold.customer AS
+SELECT *
+FROM OPENROWSET(
+    BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Customer/',
+    FORMAT = 'PARQUET'
+) AS CustomerQuery;
 
 
-CREATE VIEW gold.ProductCategory
-AS
-SELECT 
-    *
-FROM
-    OPENROWSET(
-        BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_ProductCategory/',
-        FORMAT = 'PARQUET'
-    ) AS CategoryQuery
+-- Product Category View
+CREATE VIEW gold.ProductCategory AS
+SELECT *
+FROM OPENROWSET(
+    BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_ProductCategory/',
+    FORMAT = 'PARQUET'
+) AS CategoryQuery;
 
 
-CREATE VIEW gold.ProductSubcategory
-AS
-SELECT 
-    *
-FROM
-    OPENROWSET(
-        BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_ProductSubcategory/',
-        FORMAT = 'PARQUET'
-    ) AS SubCategoryQuery
+-- Product Subcategory View
+CREATE VIEW gold.ProductSubcategory AS
+SELECT *
+FROM OPENROWSET(
+    BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_ProductSubcategory/',
+    FORMAT = 'PARQUET'
+) AS SubCategoryQuery;
 
 
-CREATE VIEW gold.Products
-AS
-SELECT 
-    *
-FROM
-    OPENROWSET(
-        BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Products/',
-        FORMAT = 'PARQUET'
-    ) AS ProductsQuery
+-- Products View
+CREATE VIEW gold.Products AS
+SELECT *
+FROM OPENROWSET(
+    BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Products/',
+    FORMAT = 'PARQUET'
+) AS ProductsQuery;
 
 
-CREATE VIEW gold.Returns
-AS
-SELECT 
-    *
-FROM
-    OPENROWSET(
-        BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Returns/',
-        FORMAT = 'PARQUET'
-    ) AS ReturnsQuery
+-- Returns View
+CREATE VIEW gold.Returns AS
+SELECT *
+FROM OPENROWSET(
+    BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Returns/',
+    FORMAT = 'PARQUET'
+) AS ReturnsQuery;
 
 
-CREATE VIEW gold.Sales
-AS
-SELECT 
-    *
-FROM
-    OPENROWSET(
-        BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Sales/',
-        FORMAT = 'PARQUET'
-    ) AS SalesQuery
+-- Sales View
+CREATE VIEW gold.Sales AS
+SELECT *
+FROM OPENROWSET(
+    BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Sales/',
+    FORMAT = 'PARQUET'
+) AS SalesQuery;
 
 
-CREATE VIEW gold.Territories
-AS
-SELECT 
-    *
-FROM
-    OPENROWSET(
-        BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Territories/',
-        FORMAT = 'PARQUET'
-    ) AS TerritoriesQuery
-
-
-
-
-
+-- Territories View
+CREATE VIEW gold.Territories AS
+SELECT *
+FROM OPENROWSET(
+    BULK 'https://adventureworksdatalake47.dfs.core.windows.net/silver/AdventureWorks_Territories/',
+    FORMAT = 'PARQUET'
+) AS TerritoriesQuery;
